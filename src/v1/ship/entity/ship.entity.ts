7bcaa1eaa835617b56b253fc52d250c2665cde88
Matys <matus.radev@bitbit.sk>
2022-07-04 02:01:48 +0200

    docs: Add swagger docs
    [x] Add OpenAPI decorators to ShipController
    [x] Add OpenAPI decorators to ShipEntity
    [x] Add OpenAPI decorators to AddShipDto

--- src/v1/ship/entity/ship.entity.ts
+++ src/v1/ship/entity/ship.entity.ts
@@ -1,7 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class ShipEntity {
+  @ApiProperty({
+    type: Number,
+    description: "ID of the ship",
+    example: 1
+  })
   id: number;
 
+  @ApiProperty({
+    type: String,
+    description: "Name of the ship",
+    example: "Shadow of the Lost Sailor"
+  })
   name: string;
 
+  @ApiProperty({
+    type: String,
+    description: "Speed of the ship",
+    example: "31"
+  })
   speed: string;
 }
